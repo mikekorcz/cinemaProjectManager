38ae85b1d505e046690e35a9e341267f8ed6d53a
Michal <mkorcz01@gmail.com>
2020-11-06 21:02:29 +0100

    Added ticket price and booked seat after typing row and seat

--- src/main/java/cinema/Cinema.java
+++ src/main/java/cinema/Cinema.java
@@ -5,35 +5,78 @@ import java.util.Scanner;
 public class Cinema {
     public static void main(String[] args) {
 
-//        // STAGE 1/5 = creating cinema theatre
-//        System.out.println("Cinema:");
-//        System.out.println("  1 2 3 4 5 6 7 8");
-//        for (int i = 1; i <= 7; i++)
-//            System.out.println(i + " " + "S ".repeat(8).trim());
 
         Scanner scanner = new Scanner(System.in);
+
 
         System.out.println("Enter the number of rows:");
         int numberOfRows = scanner.nextInt();
         System.out.println("Enter the number of seats in each row:");
         int numberOfSeats = scanner.nextInt();
-        int totalCost = numberOfRows * numberOfSeats;
 
-        if (totalCost <= 60) {
-            totalCost *= 10;
-        } else if (totalCost > 60) {
-            if (numberOfRows % 2 == 0) {
-                int frontRows = (numberOfRows / 2 * numberOfSeats) * 10;
-                int backRows = (numberOfRows / 2 * numberOfSeats) * 8;
-                totalCost = frontRows + backRows;
-            } else if (numberOfRows % 2 != 0) {
-                int frontRows = ((numberOfRows - 1) / 2 * numberOfSeats) * 10;
-                int backRows = ((numberOfRows + 1) / 2 * numberOfSeats) * 8;
-                totalCost = frontRows + backRows;
+        System.out.println();
+        System.out.println("Cinema:");
+        for (int i = 0; i <= numberOfSeats; i++) {
+            if (i == 0) {
+                System.out.print("  ");
+                continue;
             }
+            System.out.print(i + " ");
         }
-        System.out.println("Total income:");
-        System.out.println("$" + totalCost);
+        System.out.println();
 
+        for (int j = 1; j <= numberOfRows; j++) {
+            System.out.print(j + " ");
+            for (int k = 1; k <= numberOfSeats; k++) {
+                System.out.print("S ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println("Enter a row number:");
+        int rowNumber = scanner.nextInt();
+        System.out.println("Enter a seat number in that row:");
+        int seatNumber = scanner.nextInt();
+        System.out.println();
+
+        int totalCost = numberOfRows * numberOfSeats;
+        int ticketTenDollars = 10;
+        int ticketEightDollars = 8;
+
+        if (totalCost < 60) {
+            System.out.println("Ticket price: " + "$" + ticketTenDollars);
+        } else if (totalCost > 60) {
+
+            if (rowNumber <= numberOfRows / 2) {
+                System.out.println("Ticket price: " + "$" + ticketTenDollars);
+            } else {
+                System.out.println("Ticket price: " + "$" + ticketEightDollars);
+            }
+
+            System.out.println();
+        }
+
+        System.out.println("Cinema:");
+        for (int i = 0; i <= numberOfSeats; i++) {
+            if (i == 0) {
+                System.out.print("  ");
+                continue;
+            }
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        for (int j = 1; j <= numberOfRows; j++) {
+            System.out.print(j + " ");
+            for (int k = 1; k <= numberOfSeats; k++) {
+                if (j == rowNumber && k == seatNumber) {
+                    System.out.print("B ");
+                    continue;
+                }
+                System.out.print("S ");
+            }
+            System.out.println();
+        }
     }
 }
